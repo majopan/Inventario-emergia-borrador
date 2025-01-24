5f7f257896a503039c91cb672639fade52fc0269
Maria Jose Jimenez Diaz <mariazzz2326@gmail.com>
2025-01-24 16:02:21 -0500

    se añade el login que se usara

---
+++ frontend/frontend/src/components/ForgotPassword.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { ReactComponent as Logo } from '../assets/logo.svg';
+import '../styles/ForgotPassword.css';
+
+const ForgotPassword = ({ onBackToLogin }) => {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Enviando instrucciones a ${email}`);
+  };
+
+  return (
+    <div className="form-container forgot-password">
+      <form onSubmit={handleSubmit} className="formw">
+        <Logo className="logo" style={{ width: '220px', height: 'auto', padding: '10px' }} />
+        <h4>Restablecer tu contraseña</h4>
+        <input
+          type="email"
+          placeholder="Introduce tu correo"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit">Enviar</button>
+        <a href="#" onClick={onBackToLogin}>Volver al inicio de sesión</a>
+      </form>
+    </div>
+  );
+};
+
+export default ForgotPassword;
---
+++ frontend/frontend/src/components/login.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { ReactComponent as Logo } from '../assets/logo.svg';
+import EInventoryLogo from '../assets/E-Inventory.png';
+import '../styles/Login.css';
+
+import ForgotPassword from './ForgotPassword';
+
+const Login = () => {
+  const [forgotPassword, setForgotPassword] = useState(false);
+
+  const handleForgotPasswordClick = () => {
+    setForgotPassword(true);
+  };
+
+  const handleBackToLoginClick = () => {
+    setForgotPassword(false);
+  };
+
+  return (
+    <div className="container">
+      {!forgotPassword ? (
+        <div className="form-container sign-in">
+          <form>
+            <Logo className="logo" style={{ width: '220px', height: 'auto', padding: '10px' }} />
+            <span>Iniciar sesión</span>
+            <input type="username" placeholder="Nombre de usuario" />
+            <input type="password" placeholder="Contraseña" />
+            <a href="#" onClick={handleForgotPasswordClick}>¿Olvidaste tu contraseña?</a>
+            <button type="submit">Entrar</button>
+          </form>
+        </div>
+      ) : (
+        <ForgotPassword onBackToLogin={handleBackToLoginClick} />
+      )}
+
+      <div className="toggle-container">
+        <div className="toggle">
+          <div className="toggle-panel toggle-right">
+            <img src={EInventoryLogo} alt="Logo de E-Inventory" className="logo-e" style={{ width: '300px', height: 'auto' }} />
+            <p>sistema de inventario y control</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
