e7a84a1e85f957c440da8292f66d0d811354fea2
pannzzz <mariazzz2326@gmail.com>
2025-01-25 23:19:37 -0500

    se agrega el npm install react-router-dom

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
-import Login from './components/login';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/login";
+import ForgotPassword from "./components/ForgotPassword";
+
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        {/* Ruta para la página de inicio de sesión */}
+        <Route path="/" element={<Login />} />
+
+        {/* Ruta para la página de "Olvidé mi contraseña" */}
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
+    </Router>
   );
 }
 
