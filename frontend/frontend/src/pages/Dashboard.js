6b212f19fb58d6143e0955dc4d619b4be12ba01a
Maria Jose Jimenez Diaz <mariazzz2326@gmail.com>
2025-01-30 16:28:58 -0500

    estilos

--- frontend/frontend/src/pages/Dashboard.js
+++ frontend/frontend/src/pages/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SidebarMenu from '../components/SidebarMenu';
 import DashboardContent from '../components/Dashboardcard';
 import Grafica from '../components/Grafica';
+import MachineStats from '../components/MachineStats'; // Importa el nuevo componente
 import '../styles/Dashboard.css';
 
 const Dashboard = () => {
@@ -11,7 +12,10 @@ const Dashboard = () => {
       <div className="main-container">
         <div className="dashboard-container">
           <DashboardContent />
-          <Grafica />
+          <div className="chart-and-stats-container">
+            <Grafica />
+            <MachineStats />
+          </div>
         </div>
       </div>
     </>
